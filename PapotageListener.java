475e17f4bc62e3d142b0c5220ab6bb10f089f592
snonow <arnowilhelm3@icloud.com>
2024-05-29 19:16:27 +0200

    rename photos et update rapport

--- PapotageListener.java
+++
@@ -1,7 +0,0 @@
-import java.util.EventListener;
-
-// Définition de l'interface PapotageListener
-interface PapotageListener extends EventListener {
-    public void receivePapotage(PapotageEvent event);
-}
-
